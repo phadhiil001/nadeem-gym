f6edb21b716b03b9c720f21ec900710c9f5bd656
phadhiil001 <phadhiil001@gmail.com>
2024-10-04 12:32:13 -0500

    Added the sets function and views to show the reps, tempo and rest count

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -8,7 +8,7 @@ const Workout = ({workout}) => {
         <div className="flex flex-col gap-4">
             {workout.map((exercise, i) => {
                 return (
-                    <ExerciseCard index={i} exercise={exercise} key={i} />
+                    <ExerciseCard i={i} exercise={exercise} key={i} />
                 )
             })}
         </div>
